407a7f0f42dbea246e4521f5ce6fcf51942134a2
huieunkim-dev <huieunkim.dev@gmail.com>
2026-04-15 11:09:32 +0900

    refactor: 불필요한 초기 코드 제거

--- Week2/huieunKim/src/App.jsx
+++ Week2/huieunKim/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import Header from './components/Header';
 import About from './components/About';
 import Hobby from './components/Hobby';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
